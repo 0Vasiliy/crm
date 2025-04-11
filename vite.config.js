21c9e36bce2f9af557ee1e46eae443b98d095f19
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-04-12 02:21:49 +0300

    из vite.config лишнее

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { computed } from 'vue'
-import { useUtilitiesStore } from '../stores/utilities'
 
-// https://vite.dev/config/
 export default defineConfig({
   // base: '/crm/', //Для деплоя
   plugins: [vue()],
 })
 
-const utilitiesStore = useUtilitiesStore()
-const utilities = computed(() => utilitiesStore.utilities)
+
