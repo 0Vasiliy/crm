462da3ecd688c5d45d7168a2d53457f67900e6b0
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-04-15 20:04:57 +0300

    Редактирование vite. config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  // base: '/crm/', //Для деплоя
+  base: '/crm/', //Для деплоя
   plugins: [vue()],
 })
 
